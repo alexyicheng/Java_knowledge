b04427179417dd3917f0463c2be2ffdde25c2d2c
AY <AY@DESKTOP-J2R47NB.fritz.box>
2022-08-01 14:48:34 +0200

    update it

---
+++ src/MyFirstJavaGithubProject.java
@@ -0,0 +1,16 @@
+
+public class MyFirstJavaGithubProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// That is my first java Github Project
+		
+		String value = "This is my first Java Project on Github.";
+		
+		System.out.println(value);
+		
+		
+	}
+
+}
